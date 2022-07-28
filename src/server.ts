7b76855c4b857f30cf583dd44ba3b06b293a319a
JayEs23 <jsamuel2580@gmail.com>
2022-07-28 16:26:54 +0100

    First commit

---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    try {
+      let myURL = req.query.url;
+      var pattern = new RegExp('^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.?)+[a-z]{2,}|(\?[;&amp;a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$','i');
+      var new_res = pattern.test(myURL);
+      res.send(`try GET /filteredimage?image_url={{}} ${new_res}`)
+    } catch (error) {
+      res.send(error);
+    }
+    
+  } );
+
+  app.get('/filteredimage/',function (req, res) {
+    let url = req.query.image_url;
+    const result = filterImageFromURL(url);
+    res.send(result);
+    
+  })
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
+
